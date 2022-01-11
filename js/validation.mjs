801ac39239a710dfeb914ac85d58613fa579971c
tubi55 <tubi55@nate.com>
2022-01-11 13:33:15 +0900

    이메일 인증함수

---
+++ js/validation.mjs
@@ -0,0 +1,49 @@
+const form = document.querySelector('#member');
+const btnSubmit = form.querySelector('input[type=submit]');
+
+btnSubmit.addEventListener('click', e=>{
+    if( !isTxt('userid', 5)) e.preventDefault();
+    if( !isTxt('comments', 10)) e.preventDefault();
+    if( !isEmail('email', 10)) e.preventDefault();
+})
+
+function isTxt(name, len){
+    const input = form.querySelector(`[name=${name}]`);
+    const txt = input.value;
+    
+    if(txt.length > len){
+        const errMsgs = input.closest('td').querySelectorAll('p');
+        if(errMsgs.length > 0) input.closest('td').querySelector('p').remove();
+        return true;
+    }else{
+        const errMsgs = input.closest('td').querySelectorAll('p');
+        if(errMsgs.length>0) input.closest('td').querySelector('p').remove();
+
+        const errMsg = document.createElement('p');
+        errMsg.append(`텍스트를 ${len}글자이상 입력하세요`);
+        input.closest('td').append(errMsg);
+
+        return false;
+    }    
+}
+
+function isEmail(name,len){
+    const input = form.querySelector(`[name=${name}]`);
+    const txt = input.value;
+
+    if(txt.length > len && /@/.test(txt)){
+        const errMsgs = input.closest('td').querySelectorAll('p');
+        if(errMsgs.length >0) input.closest('td').querySelector('p').remove();
+
+        return true;
+    }else{
+        const errMsgs = input.closest('td').querySelectorAll('p');
+        if(errMsgs.length >0) input.closest('td').querySelector('p').remove();
+
+        const errMsg= document.createElement('p');
+        errMsg.append(`@를 포함한 메일주소를 ${len}글자 이상 입력하세요.`);
+        input.closest('td').append(errMsg);
+
+        return false;
+    }
+}
